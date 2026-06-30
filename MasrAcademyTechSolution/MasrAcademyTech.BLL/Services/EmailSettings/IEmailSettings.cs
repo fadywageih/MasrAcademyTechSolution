b7df37914bfa5f40123072bf740d137fd4332e38
fady <fadywageih14@gmail.com>
2026-06-30 17:25:16 +0300

    Remove duplicate folder

--- MasrAcademyTechSolution/MasrAcademyTech.BLL/Services/EmailSettings/IEmailSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MasrAcademyTech.BLL.Services.EmailSettings
-{
-    public interface IEmailSettings
-    {
-        void SendEmail(Email email);
-    }
-}
--- MasrAcademyTechSolution/MasrAcademyTech.BLL/Services/Payment/IPaymentService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MasrAcademyTech.BLL.Services.Payment
-{
-    public interface IPaymentService
-    {
-        Task<string?> CreatePaymentIntent(decimal amount, string orderId, string paymentMethod = "card");
-    }
-}
